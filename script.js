3a23648fb466d4f0f5260d968979a1984d6dfa95
Pc-Beast <aaartem253@gmail.com>
2026-06-10 13:54:49 +0300

    Add product fetching and rendering functionality
    Fetch products from product.json and display them in the HTML.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+fetch('product.json')
+  .then(response => response.json())
+  .then(products => {
 
+    const container = document.getElementById('products');
+
+    products.forEach(product => {
+
+      container.innerHTML += `
+        <div class="card">
+          <img src="${product.image}" alt="">
+          <h2>${product.name}</h2>
+          <p>${product.description}</p>
+          <div class="price">${product.price}</div>
+        </div>
+      `;
+
+    });
+
+  });
