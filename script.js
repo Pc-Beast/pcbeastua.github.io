7fb495b2d57f4ee36c7525eccb26c3762c973d5f
Pc-Beast <aaartem253@gmail.com>
2026-06-10 14:02:51 +0300

    Update product display to static content
    const container = document.getElementById('products');
    
    container.innerHTML = `
      <div>
        <h2>RTX 3060</h2>
        <p>12000 грн</p>
      </div>
    `;

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-fetch('product.json')
-  .then(response => response.json())
-  .then(products => {
+const container = document.getElementById('products');
 
-    const container = document.getElementById('products');
-
-    products.forEach(product => {
-
-      container.innerHTML += `
-        <div class="card">
-          <img src="${product.image}" alt="">
-          <h2>${product.name}</h2>
-          <p>${product.description}</p>
-          <div class="price">${product.price}</div>
-        </div>
-      `;
-
-    });
-
-  });
+container.innerHTML = `
+  <div>
+    <h2>RTX 3060</h2>
+    <p>12000 грн</p>
+  </div>
+`;
